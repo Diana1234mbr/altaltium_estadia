8e35b965704943b41cf2a5f62298c8e3c515b67d
DianaMonserrat <dianamonserratbandarayo@gmail.com>
2025-06-29 19:24:41 -0600

    Cambios para servidor1.5

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -130,6 +130,7 @@ if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_URL = '/signin'
 
